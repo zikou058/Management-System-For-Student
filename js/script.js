a55e580e7ef8e3d6729886beba46caf683a594c2
zikou058 <rajawizakaria546@gmail.com>
2026-01-25 16:51:34 +0100

    add fe la zone de click fe paddying case the tb

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ import Etudiant from "./etudiants.js";
 Etudiant.MAX_NOTE = 20
 
 //console.log(Etudiant.allEtudiants())
-
+let isLoad = false;
 let filterBySettings = {
-    'column' : 'age',
+    'column' : 'id',
     'desc' :true
 }
 
@@ -14,12 +14,6 @@ const displayEtudiants = async function() {
         // sorting by 
         console.log(response)
         response.sort((a,b) => {
-            // sorting by string : 
-            if (filterBySettings.desc) {
-                return b[filterBySettings.column].localeCompare(a[filterBySettings.column])
-            }
-            return a[filterBySettings.column].localeCompare(b[filterBySettings.column]) 
-            
             // sorting by number :
             const number = typeof a[filterBySettings.column] === 'number' 
             if (number) {
@@ -28,6 +22,12 @@ const displayEtudiants = async function() {
                 }
                 return a[filterBySettings.column] - b[filterBySettings.column]
             }
+            // sorting by string : 
+            if (filterBySettings.desc) {
+                    return b[filterBySettings.column].localeCompare(a[filterBySettings.column])
+                    //console.log(b[filterBySettings.column].localeCompare(a[filterBySettings.column]))
+                }
+                return a[filterBySettings.column].localeCompare(b[filterBySettings.column]) 
         })
         // Mapping Objects to Classes.
         // .map kajibe data men response 
@@ -40,7 +40,7 @@ const displayEtudiants = async function() {
                 <tr>
                     <td>${id}</td>
                     <td>${etudiant.name}</td>
-                    <td>${etudiant.getAge()}</td>
+                    <td>${etudiant.getAge()} Age</td>
                     <td><span class="${etudiant.isAdmitted() ? 'bg-green-500' : 'bg-red-500'} text-white px-2 py-1 rounded-xl">${(etudiant.note.length > 2 ) ? etudiant.note : etudiant.note}/${Etudiant.MAX_NOTE}</span></td>
                     <td><button class="delete my-1 bg-red-600 text-white px-1 py-0.5 rounded-lg shadow hover:bg-red-500 active:scale-95 transition-all" data-id="${id}" >Supprimer</button></td>
                 </tr>
@@ -78,6 +78,10 @@ const renderEtudiants = function(){
         displayEtudiants().then((data) => {
             body.innerHTML = data.join('') // hadi helte liya wahed problem de " , "
             init()// to reinitialize the delete button event listener 
+            if (isLoad) {
+                init()
+                isLoad = true 
+            }
         })
 }
 //this's pointeur ka pointe sur HTML btn : 
@@ -94,23 +98,25 @@ const init = function(){
         })
     })
     const addfnt = document.getElementById('add')
-    addfnt.addEventListener('click' ,(event) => {
+    addfnt.addEventListener('click' ,(event) => { Note 
         addEtudiant(event)
     })
     const sortelement = document.querySelectorAll('.sort-element')
     console.log(sortelement)
     sortelement.forEach((button) =>{
         button.addEventListener('click',() => {
+            ChangeSort()
             filterBySettings.column = button.dataset.column 
-            rederSort(filterBySettings.column)
+            //rederSort(filterBySettings.column)
         })
     })
 }
 window.rederSort = (column) => {
    if (filterBySettings.column === column) {  
     const element = document.querySelector('.sort-'+column)
-    element.innerHTML = `<button class="py-1 px-1 ml-2 text-sm text-black bg-red-400 rounded-sm" onClick='ChangeSort()' >${filterBySettings.desc ?'↓':'↑'}</button>`
-   }
+    element.innerHTML = `<button class="py-1 px-1 ml-2 text-sm text-black" >${filterBySettings.desc ?'↓':'↑'}</button>`
+    //console.log(element)
+    }
 }
 rederSort('id')
 rederSort('name')
@@ -125,7 +131,6 @@ window.ChangeSort = () => {
     renderEtudiants()
     rederSort(filterBySettings.column) //hadi hiya li ka tghere li mn decroissant l' croissant
 }
-
 renderEtudiants()
 
 
